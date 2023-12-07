b1b96fc0b7ca4b40738d084132bc139b83bfc184
Pablo Bestani <pablobestanimouk@gmail.com>
2023-12-07 15:16:23 -0300

    feat(validation): added DTO validations

--- src/tasks/dto/task.dto.ts
+++ src/tasks/dto/task.dto.ts
@@ -1,12 +1,33 @@
-import { TaskStatus } from "../task.entity"
+import { TaskStatus } from "../task.entity";
+import { IsIn, IsNotEmpty, IsOptional, IsString, MinLength } from 'class-validator';
 
 export class CreateTaskDto {
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(3)
     title: string
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(10)
     description: string
 }
 
 export class UpdateTaskDto {
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(3)
+    @IsOptional()
     title?: string
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(10)
+    @IsOptional()
     description?: string
+
+    @IsIn([TaskStatus.PENDING, TaskStatus.IN_PROGRESS, TaskStatus.DONE])
+    @IsNotEmpty()
+    @IsOptional()
     status?: TaskStatus
 }
